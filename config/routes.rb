5c3066e042accde757bf315499cfcef91105b715
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2014-04-09 16:56:23 +1100

    client generated with yeoman

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Shareup::Application.routes.draw do
-  root to: 'welcome#index'
-  get '/dashboard' => 'welcome#dashboard'
-
   devise_for :users,
     controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
